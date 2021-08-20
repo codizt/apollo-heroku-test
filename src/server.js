523275a04cec5fb51998693689885ee98ba29bf4
Dev <thecodizt@gmail.com>
2021-08-20 13:14:51 +0530

    Changed landing page

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 const {ApolloServer} = require('apollo-server')
+const {ApolloServerPluginLandingPageGraphQLPlayground} = require('apollo-server')
 const {typeDefs, resolvers} = require('./schema')
 
 const port = process.env.PORT || 4000
 
-const server = new ApolloServer({typeDefs, resolvers})
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers,
+    plugins: [
+        ApolloServerPluginLandingPageGraphQLPlayground()
+    ]
+})
 
 server.listen({port}, ()=> {
     console.log(`Sever ready at http://localhost:${port}/graphql`);
